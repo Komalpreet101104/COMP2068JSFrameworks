ab27db73b359ce89ad85d9ec262bc40c0d3e2084
Komalpreet101104 <komalpreet101104@gmail.com>
2024-12-06 19:39:52 -0500

    added images

--- ASSIGNMENT02/PracticeActivity/routes/index.js
+++ ASSIGNMENT02/PracticeActivity/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// In routes/index.js (or app.js)
+
+router.get('/', (req, res) => {
+  // Define categories with name and image URL
+  const categories = [
+    { name: 'Fruits', image: '/images/fruits.jpg' },
+    { name: 'Vegetables', image: '/images/vegetables.jpg' },
+    { name: 'Dairy', image: '/images/dairy.jpg' },
+    { name: 'Bakery', image: '/images/bakery.jpg' }
+  ];
+
+  // Render the index page with the categories data
+  res.render('index', { categories });
+});
+
+
 module.exports = router;
